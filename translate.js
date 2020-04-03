12b18c4426a77167a26c819f7afd77e8912328ef
Men-club <63089830+Men-club@users.noreply.github.com>
2020-04-03 10:06:24 +0400

    1
    1

---
+++ translate.js
@@ -0,0 +1,197 @@
+var new_lang = {
+
+    "lt0": "XX<span>Monóculo</span>",
+
+    "lt1": "Aumento<br>16x",
+
+    "lt2": "Imagen<br>de alta calidad",
+
+    "lt3": "Cuerpo antichoque<br>e impermeable",
+
+    "lt4": "La oferta termina dentro:",
+
+    "lt5": "precio<br>anterior:",
+
+    "lt6": "euros",
+
+    "lt7": "precio<br>con descuento:",
+
+    "lt8": "euros",
+
+    "lt9": "Pedir con un descuento",
+
+    "lt10": "XX <span>Monóculo</span>",
+
+    "lt11": "La oferta termina dentro:",
+
+    "lt12": "Aumento<br>16x",
+
+    "lt13": "Imagen<br>de alta calidad",
+
+    "lt14": "Cuerpo antichoque<br>e impermeable",
+
+    "lt15": "precio<br>anterior:",
+
+    "lt16": "euros",
+
+    "lt17": "precio<br>con descuento:",
+
+    "lt18": "euros",
+
+    "lt19": "Pedir con un descuento",
+
+    "lt20": "Monóculo<br><span>ultra potente</span><br><span>de la última generación</span>",
+
+    "lt21": "No te perderás<br><span>ni un detalle</span>",
+
+    "lt22": "¿Vas a la naturaleza muchas veces?<br>¿Te gusta vigilar los objetos remotos?<b>Los cazadores con experiencia y los adeptos de las actividades al aire libre</b>amarán este monóculo.",
+
+    "lt23": "¿Dónde<br>te servirá<span>el monóculo</span><br>XX?",
+
+    "lt24": "Pedir con un descuento",
+
+    "lt25": "si estás de caza",
+
+    "lt26": "encontrarás la pista<br>en un instante",
+
+    "lt27": "si estás en la naturaleza",
+
+    "lt28": "disfrutarás<br>los paisajes maravillosos",
+
+    "lt29": "si estás en el estadio",
+
+    "lt30": "podrás elegir asiento en las últimas filas<br>del estadio de fútbol",
+
+    "lt31": "Pedir con un descuento",
+
+    "lt32": "¿Dónde<br>te servirá<span>el monóculo</span><br>XX?",
+
+    "lt33": "si estás de caza",
+
+    "lt34": "encontrarás la pista<br>en un instante",
+
+    "lt35": "si estás en la naturaleza",
+
+    "lt36": "disfrutarás<br>los paisajes maravillosos",
+
+    "lt37": "si estás en el estadio",
+
+    "lt38": "podrás elegir asiento en las últimas filas<br>del estadio de fútbol",
+
+    "lt39": "¿Por qué<br><span class=\"span\">elegir</span><span>XX?</span>",
+
+    "lt40": "Muy fácil de usar",
+
+    "lt41": "Es un dispositivo compacto y ligero",
+
+    "lt42": "Tiene un zoom ultra potente",
+
+    "lt43": "Se ajusta de dos maneras",
+
+    "lt44": "Pedir con un descuento",
+
+    "lt45": "Reseñas<br><span>de los clientes</span><span class=\"white\">contentos</span>",
+
+    "lt46": "Andrés, Madrid",
+
+    "lt47": "Me compré XX a un precio que me parece módico que corresponde a la buena calidad del dispositivo. Me ayuda a cazar, casi siempre vuelvo con algo. Estoy muy contento",
+
+    "lt48": "Jorge, Zaragoza",
+
+    "lt49": "El precio corresponde a su calidad, es compacto y resistente. Antes tenía uno que me habían regalado. Pero luego se cayó en la piedra y se rompió. Mi nuevo monóculo XX se ha caído ya un par de veces, pero no le ha pasado nada. Creo que el monóculo ha sido una buena adquisición.",
+
+    "lt50": "Vicente, Pontevedra",
+
+    "lt51": "Soy un alpinista con experiencia y he visto muchos monóculos, pero XX es el mejor, creo yo. La calidad es excelente y el precio es bien bajo. Pero es que lamentaría perderlo.",
+
+    "lt52": "Andrés, Madrid",
+
+    "lt53": "Me compré XX a un precio que me parece módico que corresponde a la buena calidad del dispositivo. Me ayuda a cazar, casi siempre vuelvo con algo. Estoy muy contento",
+
+    "lt54": "Jorge, Zaragoza",
+
+    "lt55": "El precio corresponde a su calidad, es compacto y resistente. Antes tenía uno que me habían regalado. Pero luego se cayó en la piedra y se rompió. Mi nuevo monóculo XX se ha caído ya un par de veces, pero no le ha pasado nada. Creo que el monóculo ha sido una buena adquisición.",
+
+    "lt56": "Vicente, Pontevedra",
+
+    "lt57": "Soy un alpinista con experiencia y he visto muchos monóculos, pero XX es el mejor, creo yo. La calidad es excelente y el precio es bien bajo. Pero es que lamentaría perderlo.",
+
+    "lt58": "¡Pedir<br><span>el monóculo</span><span class=\"white-b\">es muy fácil!</span>",
+
+    "lt59": "Solicitud",
+
+    "lt60": "Deja una solicitud en el sitio web<br>rellenando el formulario",
+
+    "lt61": "Confirmación",
+
+    "lt62": "Nuestro especialista te llamará<br>para confirmar el pedido",
+
+    "lt63": "Recibo",
+
+    "lt64": "Te entregaremos el pedido<br>del modo más conveniente para ti",
+
+    "lt65": "Pago",
+
+    "lt66": "Pagarás<br>en efectivo o con tarjeta en el momento de recibir el pedido",
+
+    "lt67": "XX<span>Monóculo</span>",
+
+    "lt68": "Aumento<br>16x",
+
+    "lt69": "Imagen<br>de alta calidad",
+
+    "lt70": "Cuerpo antichoque<br>e impermeable",
+
+    "lt71": "La oferta termina dentro:",
+
+    "lt72": "precio<br>anterior:",
+
+    "lt73": "euros",
+
+    "lt74": "precio<br>con descuento:",
+
+    "lt75": "euros",
+
+    "lt76": "Madrid",
+
+    "lt77": "PEDIR CON UN DESCUENTO",
+
+    "lt79": "XX<span>Monóculo</span>",
+
+    "lt80": "La oferta termina dentro:",
+
+    "lt81": "Aumento<br>16x",
+
+    "lt82": "Imagen<br>de alta calidad",
+
+    "lt83": "Cuerpo antichoque<br>e impermeable",
+
+    "lt84": "precio<br>anterior:",
+
+    "lt85": "euros",
+
+    "lt86": "precio<br>con descuento:",
+
+    "lt87": "euros",
+
+    "lt88": "Madrid",
+
+    "lt89": "PEDIR CON UN DESCUENTO",
+
+    "lt91": "Política de privacidad",
+
+    "lt92": "Oferta pública"
+};
+
+function Translater() {
+    for (class_name in new_lang) {
+        var elements = document.getElementsByClassName(class_name);
+        if (elements.length) {
+            for (key in elements) {
+                elements[key].innerHTML = new_lang[class_name];
+            }
+        }
+    }
+}
+;
+
